eddede16d1dc50bb4a86b1ed10164aac8c985f94
b6320502452 <pavich.s@ku.th>
2021-03-01 17:46:19 +0700

    Create lab4.3.c

---
+++ lab4.3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char text[201];
+    gets(text);
+    for(int i=0;i<sizeof text;i++)
+    {
+        while(text[i]==text[i+1])
+        {
+
+        }
+    }
+}
